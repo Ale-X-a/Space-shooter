9b4f3b5a5da043fdfbc9eb1e1f42bdf2994122a1
Alexandra Padurean <208801745+Ale-X-a@users.noreply.github.com>
2025-06-16 12:39:27 +0200

    Initial commit

---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    [SerializeField] WaveConfigSO waveConfig;
+    List<Transform> waypoints;
+    int waypointIndex = 0;
+
+    public void SetWaveConfig(WaveConfigSO config)
+    {
+        waveConfig = config;
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].position;
+    }
+    void Start()
+    {
+        if (waveConfig != null)
+        {
+            waypoints = waveConfig.GetWaypoints();
+            transform.position = waypoints[waypointIndex].position;
+        }
+    }
+    void Update()
+    {
+        if (waypoints == null) return; 
+        FollowPath();
+    }
+
+    void FollowPath()
+    {
+        if (waypointIndex < waypoints.Count)
+        {
+            Vector3 targetPosition = waypoints[waypointIndex].position;
+            float delta = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, delta);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
